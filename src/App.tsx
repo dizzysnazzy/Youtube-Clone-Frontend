e64b500457acd32937b1391ee9aab9346a4b8d2a
dizzysnazzy <dismaskipchumba2@gmail.com>
2022-07-11 12:34:26 -0700

    modification

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,25 @@
-import React, { useState } from "react";
-import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import MainPage from "./Pages/MainPage/MainPage";
-import SignInPage from "./Pages/SignInPage/SignInPage";
-import AccountPage from "./Pages/AccountPage/AccountPage";
-import Navbar from "./Components/Navbar/Navbar";
-import UploadPage from "./Pages/UploadPage/UploadPage";
-
-import { ToggleSidebarContext } from "./Helpers/Context";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [showSidebar, setShowSidebar] = useState<any>(true);
-
   return (
-    <ToggleSidebarContext.Provider value={{ showSidebar, setShowSidebar }}>
-      <div className="top">
-        <Navbar />
-      </div>
-      <Router>
-        <Switch>
-          <Route path="/" exact render={() => <MainPage />} />
-          <Route path="/signin" exact render={() => <SignInPage />} />
-          <Route path="/account" exact render={() => <AccountPage />} />
-          <Route path="/upload" exact render={() => <UploadPage />} />
-        </Switch>
-      </Router>
-    </ToggleSidebarContext.Provider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
